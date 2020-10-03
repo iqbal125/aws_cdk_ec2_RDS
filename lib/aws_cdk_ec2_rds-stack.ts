18a685e9c429923bfffe519d816157155e325fbb
iqbal125 <iqbal125@yahoo.com>
2020-10-03 13:49:45 -0700

    added vpc

--- lib/aws_cdk_ec2_rds-stack.ts
+++ lib/aws_cdk_ec2_rds-stack.ts
@@ -1,9 +1,9 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core"
+import * as ec2 from "@aws-cdk/aws-ec2"
 
 export class AwsCdkEc2RdsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
+    super(scope, id, props)
+    const vpc = new ec2.Vpc(this, "VPC")
   }
 }
