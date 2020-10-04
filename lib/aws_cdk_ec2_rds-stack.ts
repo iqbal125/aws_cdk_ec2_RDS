dda31ef8bc0545be084678877550a20eba04726f
iqbal125 <iqbal125@yahoo.com>
2020-10-03 18:06:08 -0700

    added ci cd

--- lib/aws_cdk_ec2_rds-stack.ts
+++ lib/aws_cdk_ec2_rds-stack.ts
@@ -1,9 +1,101 @@
 import * as cdk from "@aws-cdk/core"
 import * as ec2 from "@aws-cdk/aws-ec2"
+import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2"
+import {AutoScalingGroup} from "@aws-cdk/aws-autoscaling"
+import * as codepipeline from "@aws-cdk/aws-codepipeline"
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions"
+import * as codebuild from "@aws-cdk/aws-codebuild"
+import * as codedeploy from "@aws-cdk/aws-codedeploy"
+
+require("dotenv").config()
 
 export class AwsCdkEc2RdsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    //Start stack
     const vpc = new ec2.Vpc(this, "VPC")
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, "LB", {
+      vpc,
+      internetFacing: true,
+    })
+
+    const listener = lb.addListener("Listener", {
+      port: 80,
+    })
+
+    const asg = new AutoScalingGroup(this, "ASG", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(), // get the latest Amazon Linux image
+    })
+
+    const targets = listener.addTargets("ApplicationFleet", {
+      port: 8080,
+      targets: [asg],
+    })
+
+    // Source Stage
+    const sourceOutput = new codepipeline.Artifact()
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: "GitHub_Source",
+      owner: "iqbal125",
+      repo: "react-express-sample",
+      oauthToken: process.env.GITHUB_ACCESS_TOKEN,
+      output: sourceOutput,
+    })
+
+    //Build Stage
+    const project = new codebuild.PipelineProject(this, "MyProject", {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+          build: {
+            commands: [
+              // By default, you're in a directory with the contents of the repository from sourceAction1.
+              // Use the CODEBUILD_SRC_DIR_source2 environment variable
+              // to get a path to the directory with the contents of the second input repository.
+            ],
+          },
+        },
+      }),
+    })
+
+    const buildOutput = new codepipeline.Artifact()
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: "CodeBuild",
+      project,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    })
+
+    //Deploy Stage
+    const deploymentGroup = new codedeploy.ServerDeploymentGroup(this, "DeploymentGroup", {
+      loadBalancer: codedeploy.LoadBalancer.application(targets),
+    })
+
+    const deployAction = new codepipeline_actions.CodeDeployServerDeployAction({
+      actionName: "CodeDeploy",
+      input: buildOutput,
+      deploymentGroup,
+    })
+
+    new codepipeline.Pipeline(this, "MyPipeline", {
+      stages: [
+        {
+          stageName: "Source",
+          actions: [sourceAction],
+        },
+        {
+          stageName: "Build",
+          actions: [buildAction],
+        },
+        {
+          stageName: "Deploy",
+          actions: [deployAction],
+        },
+      ],
+    })
   }
 }
